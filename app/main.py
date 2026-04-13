1aa5a563124d1da4149431319ae055eb0cdcbe1d
gunsts <137734013+gunsts@users.noreply.github.com>
2026-04-13 20:50:48 +0700

    Solution

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self) -> str:
-        return self.filename
+    def __enter__(self) -> object:
+        return self
 
     def __exit__(self, *args) -> None:
         if os.path.exists(self.filename):
